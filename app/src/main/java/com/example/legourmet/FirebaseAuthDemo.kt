faf6663226305d8fba599dfbcbbf9c69b18f0af7
Joao-r-Am <joaoric.amorim@gmail.com>
2021-07-15 20:20:02 -0300

    tela usuario -> refazer tela resultaldo -> refazer

---
+++ app/src/main/java/com/example/legourmet/FirebaseAuthDemo.kt
@@ -0,0 +1,11 @@
+package com.example.legourmet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirebaseAuthDemo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_auth_demo)
+    }
+}
